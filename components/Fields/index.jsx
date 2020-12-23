d19e182d067ac017f491effeda7f23ae198378eb
RikoGit <r.git@lenta.ru>
2020-12-23 08:23:49 +0300

    Delete index.js

--- components/Fields/index.jsx
+++ components/Fields/index.jsx
@@ -4,37 +4,46 @@ import styles from "./styles.scss";
 
 const Fields = ({ onChange, onClick }) => (
   <div className={styles.root}>
-    <input
-      className={styles.input}
-      type="number"
-      min="1"
-      max="15"
-      placeholder="width"
-      onChange={(event) => {
-        onChange("width", Number(event.target.value));
-      }}
-    />
-    <input
-      className={styles.input}
-      type="number"
-      min="1"
-      max="15"
-      placeholder="height"
-      onChange={(event) => {
-        onChange("height", Number(event.target.value));
-      }}
-    />
-    <input
-      className={styles.input}
-      type="number"
-      min="1"
-      max="15"
-      placeholder="length"
-      onChange={(event) => {
-        onChange("length", Number(event.target.value));
-      }}
-    />
-    <button type="button" onClick={onClick}>
+    <label className={styles.label}>
+      Width
+      <input
+        className={styles.input}
+        type="number"
+        min="1"
+        max="15"
+        onChange={(event) => {
+          onChange("width", Number(event.target.value));
+        }}
+      />
+      <span className={styles.text}>от 1 до 15</span>
+    </label>
+    <label className={styles.label}>
+      Height
+      <input
+        className={styles.input}
+        type="number"
+        min="1"
+        max="15"
+        onChange={(event) => {
+          onChange("height", Number(event.target.value));
+        }}
+      />
+      <span className={styles.text}>от 1 до 15</span>
+    </label>
+    <label className={styles.label}>
+      Length
+      <input
+        className={styles.input}
+        type="number"
+        min="1"
+        max="15"
+        onChange={(event) => {
+          onChange("length", Number(event.target.value));
+        }}
+      />
+      <span className={styles.text}>от 1 до 15</span>
+    </label>
+    <button type="button" onClick={onClick} className={styles.button}>
       Display 3D box in 3D view
     </button>
   </div>
--- components/Loader/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import styles from "./styles.scss";
-
-const Loader = () => <div className={styles.loader}></div>;
-
-export default Loader;
